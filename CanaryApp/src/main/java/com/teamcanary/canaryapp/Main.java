3a7972032be0ba5146a64d57229c99338391a17a
Gustavo Zapata <k1715308@kingston.ac.uk>
2019-02-08 09:57:54 +0000

    implemented the MVC design pattern to the app

---
+++ CanaryApp/src/main/java/com/teamcanary/canaryapp/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamcanary.canaryapp;
+
+import java.awt.BorderLayout;
+import javax.swing.WindowConstants;
+import view.MainWindow;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Main {
+    public static void main(String[] args){
+        
+        //MAIN WINDOW
+        MainWindow window = new MainWindow();
+        
+        
+    }
+}
---
+++ CanaryApp/src/main/java/model/Task.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Task {
+    private String taskDescription;
+    private User owner;
+    private User assignedTo;
+    private Date date;  //or private String date;
+    private String priority;
+    private String category;
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public User getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(User assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    
+}
