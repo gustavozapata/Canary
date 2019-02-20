3caaf00dade5c2840650d87432875583eebdcc1b
Gustavo Zapata <k1715308@kingston.ac.uk>
2019-02-20 13:39:03 +0000

    created the MVC structure for the ToDo app

--- CanaryApp/src/main/java/com/teamcanary/canaryapp/Main.java
+++ CanaryApp/src/main/java/com/teamcanary/canaryapp/Main.java
@@ -1,11 +1,15 @@
 package com.teamcanary.canaryapp;
 
+import com.google.gson.Gson;
 import controller.TaskSystem;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.BoxLayout;
 import model.Task;
 import model.User;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import view.AppPanel;
 import view.AppWindow;
 import view.TasksContainerView;
@@ -14,6 +18,16 @@ import view.TodoText;
 
 public class Main {
     public static void main(String[] args){
+        
+        //------>>>>>>>> TO BE MOVED TO THE RIGHT CLASS
+        String paulJsonUrl = "http://nooblab.com/p2.json";
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(paulJsonUrl);
+
+
+        //------>>>>>>>> TO BE MOVED TO THE RIGHT CLASS
+        Gson gson = new Gson();
+        
         
         //CREATING DEFAULT TESTING TASKS
         User u1 = new User("Kylan","Haffie","Mazemace","Todocanary123");
