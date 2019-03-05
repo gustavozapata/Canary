74210f29ffd71db888e8ef5e41086408cdb3b535
Kylan <Kylan@10.130.65.8>
2019-03-05 15:29:20 +0000

    Merged Model and GUI Stuff

---
+++ CanaryApp/src/main/java/model/SubTask.java
@@ -0,0 +1,8 @@
+package model;
+
+public class SubTask extends Task {
+    public SubTask(TaskContainer taskContainer, String taskDescription, User user){ // Doesnt duplicate variables, just passes them in
+        super(taskDescription, user); // Uses superclasses constructor
+    }
+}
+
