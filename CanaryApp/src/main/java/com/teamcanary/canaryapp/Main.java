93b7967df36ef3e6aa1330decebaee449ef5ecbb
K1713627 <K1713627@kingston.ac.uk>
2019-03-28 14:32:24 +0000

    Added Reading Writing

--- CanaryApp/src/main/java/com/teamcanary/canaryapp/Main.java
+++ CanaryApp/src/main/java/com/teamcanary/canaryapp/Main.java
@@ -1,7 +1,10 @@
 package com.teamcanary.canaryapp;
 
+import controller.JsonManager;
 import controller.UserSystem;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import model.Task;
 import view.AppView;
 
 /**
@@ -9,8 +12,9 @@ import view.AppView;
  * @author k1715308
  */
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException, IOException{
         AppView mainWindow = AppView.getInstance();
         UserSystem.loadEverybody();
+       
     }
 }
